da93047c04ddd1609a7338f03dbb2771b71388ee
Sahil Pathak <143155755+sahil454521@users.noreply.github.com>
2025-01-29 12:26:56 +0530

    Your commit message

---
+++ react/app.js
@@ -0,0 +1,18 @@
+export ReadableByteStreamController from './ReadableByteStreamController.js';
+function MyButton() {
+    return (
+      <button>
+        I'm a button
+      </button>
+    );
+  }
+  
+  export default function MyApp() {
+    return (
+      <div>
+        <h1>Welcome to my app</h1>
+        <MyButton />
+      </div>
+    );
+  }
+  

f5b121b7ceef48b964a8c2e4f987af7007787c95
Sahil Pathak <143155755+sahil454521@users.noreply.github.com>
2025-02-11 22:53:05 +0530

    Add files via upload

--- WheatherAPP/wheather.js
+++ WheatherAPP/wheather.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const api_key = 'b939d537523042adb2e165139251102';
 const input = document.getElementById('city'); 
 const btn = document.getElementById('btn');
@@ -35,30 +35,3 @@ function getweather(city) {
         console.error('Error fetching weather data:', error);
     });
 }
-=======
-const api_key = '82cce4b17954e086cd57a4bdfc95cdc2';
-const input = document.querySelector('.input'); 
-const btn = document.getElementById('btn');
-const icon = document.getElementById('icon');
-const description = document.getElementById('description');
-const weather = document.getElementById('wheather');
-const temperature = document.getElementById('tempreature');
-
-btn.addEventListener('click', () => {
-    let city = input.value;
-    getweather(city);
-    
-})
-
-function getweather(city) {
-    console.log(city);
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        icon.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-        description.innerText = data.weather[0].description;
-        weather.innerText = data.weather[0].main;
-        temperature.innerText = data.main.temp;
-    })
->>>>>>> 94f7588649f3947dae8e3c1d563e0c3b9662c5e4

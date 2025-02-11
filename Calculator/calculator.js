1bb00097fc884a3df2323b99b26c18e8e6c15ed6
Sahil Pathak <143155755+sahil454521@users.noreply.github.com>
2025-02-11 22:54:46 +0530

    Update calculator.js

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 let input= document.getElementById("input");
 let button = document.querySelectorAll("button");
 
@@ -17,24 +17,4 @@ button.forEach(element => {
     }       
     input.scrollLeft = input.scrollWidth;  
     })
-=======
-let input= document.getElementById("input");
-let button = document.querySelectorAll("button");
-
-button.forEach(element => {
-    element.addEventListener("click",(e)=>{
-           console.log(e.target.textContent)
-
-    if (e.target.textContent === "C") {
-      input.innerText= "";
-    } else if (e.target.textContent === "<") {
-      input.innerText = input.innerText.slice(0, -1);
-    } else if (e.target.textContent === "=") {
-      input.innerText = eval(input.innerText);
-    } else {
-      input.innerText += e.target.textContent;
-    }       
-    input.scrollLeft = input.scrollWidth;  
-    })
->>>>>>> 94f7588649f3947dae8e3c1d563e0c3b9662c5e4
 });
